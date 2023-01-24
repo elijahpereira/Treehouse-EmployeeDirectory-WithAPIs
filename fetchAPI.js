918463bf43df6484d9fdbf45081c7c2a4b627c04
Elijah Pereira <vahpourvisuals@gmail.com>
2023-01-23 19:21:45 -0500

    syntax fixes

--- fetchAPI.js
+++ fetchAPI.js
@@ -61,13 +61,12 @@ function displayModal(index) {
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
     }
-    T
+    
 
 gridContainer.addEventListener('click', e => {
     // make sure the click is not on the gridContainer itself
     if (e.target !== gridContainer) {
-    // select the card element based on its proximity to actual element
-    clicked
+    // select the card element based on its proximity to actual element clicked
     const card = e.target.closest(".card");
     const index = card.getAttribute('data-index');
     displayModal(index);
